0f3e70495721d5a85da5bd936efb061b37f0ea7b
Mark Peters <mapeters101@earthlink.net>
2024-10-09 19:27:33 -0400

    Lesson144:  Work with sorting up to 5:30.

---
+++ sorting.py
@@ -0,0 +1,25 @@
+print(1 < 2)
+print('A' < 'Z')
+
+def key_func(x):
+    return abs(x)
+
+data = [-10, -6, 0, 3, 6]
+print(sorted(data))
+
+print([key_func(x) for x in data])
+
+print(sorted(data, key=key_func))
+
+data = [2, -2, 1, -1]
+print(sorted(data, key=key_func))
+print(sorted(data, key=key_func, reverse=True))
+print(sorted(data, key=lambda x: abs(x)))
+print(sorted(data, key=abs))
+
+
+
+
+
+
+
